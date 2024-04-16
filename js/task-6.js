b9c9d3882cf29df4e7f8908847ade8af08300e31
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-04-16 10:57:23 +0300

    перездача

--- js/task-6.js
+++ js/task-6.js
@@ -19,16 +19,19 @@ function getRandomHexColor() {
 }
 
 function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
   for (let i = 0; i < amount; i++) {
    const box = document.createElement('div');
    const size = 30 + i * 10;
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+    fragment.appendChild(box);
   }
 }
   
+boxesContainer.appendChild(fragment);
+
 function destroyBoxes() {
  boxesContainer.innerHTML = '';
 }
